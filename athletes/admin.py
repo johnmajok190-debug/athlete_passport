a4a7ccf397bfbd9b3384abd427ab357ef23fb954
Majok Pandak <johnmajok190@gmail.com>
2026-07-20 13:05:48 +0300

    Set up athlete model and project foundation

--- athletes/admin.py
+++ athletes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from athletes.models import Athlete
+
 # Register your models here.
+admin.site.register(Athlete)
--- athletes/models.py
+++ athletes/models.py
@@ -1,3 +1,79 @@
+import uuid
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Athlete(models.Model):
+
+    class Status(models.TextChoices):
+        ACTIVE = "ACTIVE", "Active"
+        INACTIVE = "INACTIVE", "Inactive"
+        SUSPENDED = "SUSPENDED", "Suspended"
+        RETIRED = "RETIRED", "Retired"
+
+    class Gender(models.TextChoices):
+        MALE = "MALE", "Male"
+        FEMALE = "FEMALE", "Female"
+        OTHER = "OTHER", "Other"
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    athlete_id = models.CharField(
+        max_length=20,
+        unique=True,
+        editable=False,
+    )
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="athlete_profile",
+    )
+
+    date_of_birth = models.DateField()
+
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+    )
+
+    height_cm = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    weight_kg = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    country = models.CharField(max_length=100, db_index=True)
+    county_or_state = models.CharField(max_length=100, null=True, blank=True)
+
+    city = models.CharField(max_length=100, db_index=True)
+
+    profile_photo = models.ImageField(
+        upload_to="athletes/photos/",
+        blank=True,
+        null=True,
+    )
+
+    bio = models.TextField(
+        blank=True,
+        max_length=500
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.ACTIVE,
+    )
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    
+def __str__(self):
+    return self.user.get_full_name() or self.user.username
